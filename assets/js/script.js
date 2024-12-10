91fa77a88f2a49a8702949686e6663eb9f49a947
Lyuuki09 <leandroyuuki@gmail.com>
2024-12-10 19:19:22 -0300

    ex validação senha pronto

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,38 @@
-// exercício 1
+// Adivinhador de número (1 a 10)
 
 function enviarResposta(){
     let resposta = Number(document.getElementById("adivinhar").value)
 
     if(resposta === 3){
-        alert("Acertou legal!")
+        document.querySelector("p").innerText = "Acertou legal!"
     }else{
-        alert("Errou, parceiro!")
+        document.querySelector("p").innerText = "Errou, parceiro!"
     }
 
     document.getElementById("adivinhar").value = ""
 }
 
-document.getElementById("adivinhar").addEventListener("keydown", function(event) {
-    // Verifica se a tecla pressionada é o Enter (código 13)
-    if (event.key === "Enter") {
-        enviarResposta(); // Chama a função quando Enter é pressionado
+// Verificador de ano bissexto
+
+function verificarAnoBissexto(){
+    let resposta = document.getElementById("verificarAnoBissexto").value
+    let parte = Number(resposta.slice(2,4))
+
+    if(parte % 4 == 0){
+        document.querySelector("p").innerText = "Este ano é bissexto!"
+    }else{
+        document.querySelector("p").innerText = "Este ano não é bissexto!"
     }
-});
+    document.getElementById("verificarAnoBissexto").value = ""
+}
+
+// Validação de senha
+
+function validadorSenha(){
+    let resposta = document.getElementById("validadorSenha").value
+    if (resposta.length < 8){
+        document.querySelector("p").innerText = "Esta senha não é válida, a senha deve ter \nno mínimo 8 caracteres!"
+    }else{
+        document.querySelector("p").innerText = "Senha válida!"
+    }
+}
